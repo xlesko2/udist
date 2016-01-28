098b5e90a4cb54c84ad66e3a0ef8ba7bc6c7aa8e
KayEl <kristian.lesko@gmail.com>
2016-01-28 15:13:28 +0100

    Merged product_type.py and product.py into product.py file (to avoid circular dependency between Product and ProductType). Completed ProductType constructor (with asserts).

--- product.py
+++ product.py
@@ -1,5 +1,30 @@
-from product_type import ProductType
 from organization import Organization
+
+class ProductType(object):
+	'''
+	Class representing a product type.
+	Product type specifies manufacture & transport requirements etc.
+	'''
+	def __init__(self, name, requirements, production_time):
+		'''
+		Params:
+			-- name - string
+			-- requirements - dict of required materials/products
+			-- production_time - base time of making a product of this type
+				(integer value)
+		'''
+		assert str(name) == name, 'Product name not a string.'
+		self.name = name
+		
+		assert isinstance(requirements, dict), 'Requirements not a dict.'
+		assert all(isinstance(i, Product) for i in requirements),\
+			'Invalid requirement (not a product).'
+		self.requirements = requirements
+		
+		assert isinstance(production_time, int), 'Non-integer production time.'
+		self.production_time = production_time
+		return None
+
 
 class Product(object):
 	'''
