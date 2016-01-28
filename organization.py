7b2245f3eb38e7d41135d0b204c955b355d9b7b0
KayEl <kristian.lesko@gmail.com>
2016-01-28 15:23:36 +0100

    Merged Factory object into organization.py, deleted factory.py.

--- organization.py
+++ organization.py
@@ -4,3 +4,17 @@ class Organization(object):
 	Virtual class inherited by Factory.
 	'''
 	pass
+
+class Factory(Organization):
+	'''
+	Class representing a single manufacturing unit.
+	The factory is placed on a certain position in the game world,
+	and manufactures a product based on its specification.
+	'''
+	def __init__(self, product_data, position):
+		'''
+		Params:
+			-- product_data - dictionary of data. So far, not sure which
+			-- position - 2D Vector that positions factory in game world
+		'''
+		pass
