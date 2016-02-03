7ffb27ca6dbe80c6b17e34db8763ee73e2b48756
KayEl <kristian.lesko@gmail.com>
2016-02-03 13:15:13 +0100

    Added a new test case (simulation of computer manufacture).

--- simulation.py
+++ simulation.py
@@ -4,14 +4,27 @@ from vector2d import Vector2D
 from world import World
 from matplotlib import pyplot
 
-textile = ProductType('Textile', {}, 0.97)
-ziemiaciek = ProductType('Ziemiaciek', {textile: 2}, 1.04)
-pluszowy_mort = ProductType('Pluszowy Mort', {textile: 2, ziemiaciek: 1}, 1.24)
-m_tex = Mine(textile, 1000, Vector2D(5,5))
-f_ziem = Factory(ziemiaciek, 400, Vector2D(7,12))
-f_mort = Factory(pluszowy_mort, 1500, Vector2D(15,7))
-c_mort = CustomerPoint(pluszowy_mort, 500, Vector2D(4,18))
-w = World((20,20), [m_tex, f_ziem, f_mort, c_mort])
+silicone = ProductType('Silicone', {}, 1.0)
+plastic = ProductType('Plastic', {}, 1.2)
+aluminium = ProductType('Aluminium', {}, 1.1)
+processor = ProductType('Processor', {silicone: 8}, 3.2)
+ram = ProductType('RAM', {silicone: 4, plastic: 2}, 1.8)
+gpu = ProductType('GPU', {silicone: 7}, 3.3)
+case = ProductType('PC Case', {aluminium: 12, plastic: 4}, 1.2)
+computer = ProductType('Computer', {processor: 1, ram: 4, gpu: 1, case: 1}, 2.7)
+
+m_sil = Mine(silicone, 1200, Vector2D(1,1))
+m_plast = Mine(plastic, 1400, Vector2D(14,0))
+m_alu = Mine(aluminium, 1500, Vector2D(15,14))
+f_proc = Factory(processor, 700, Vector2D(7,2))
+f_ram = Factory(ram, 1200, Vector2D(8,4))
+f_gpu = Factory(gpu, 800, Vector2D(6,3))
+f_case = Factory(case, 1400, Vector2D(1,6))
+f_comp = Factory(computer, 1000, Vector2D(4,12))
+c_comp = CustomerPoint(computer, 1400, Vector2D(19,17))
+
+w = World((20,20), [m_sil, m_plast, m_alu, f_proc, f_ram, f_gpu, f_case,
+	f_comp, c_comp])
 
 class Simulation(object):
 	'''
